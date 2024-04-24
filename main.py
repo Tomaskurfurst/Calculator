ad6fb86e3fdc2b056c5b25fef224c7a080e115a4
Tomaskurfurst <167827348+Tomaskurfurst@users.noreply.github.com>
2024-04-23 22:03:56 -0300

    logger não é necessário

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-from kivy.logger import Logger
 
 class MainApp(App):
     def build(self):
